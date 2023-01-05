dcec80d0fc17af6d2487971219427a58a65518c5
David Clare <david_clare@hotmail.co.uk>
2023-01-05 23:33:01 +0000

    Added many features and fixed a few bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
+from datetime import date
 import streamlit as st
 import pandas as pd
 import numpy as np
 from SPARQLWrapper import SPARQLWrapper, JSON
 import plotly.express as px
+import plotly.graph_objects as go
+
+property_types = [
+  'detached',
+  'semi-detached',
+  'terraced',
+  'flat-maisonette'
+]
 
 def get_postcode_df(postcode:str):
   # https://landregistry.data.gov.uk/app/qonsole
@@ -44,15 +53,20 @@ WHERE
   if not 'saon' in df.columns:
     df['saon'] = np.nan
   # convert numerics
-  cols = ['paon', 'amount']
-  df[cols] = df[cols].apply(pd.to_numeric, errors='ignore', axis=1)
+  df['amount'] = df['amount'].apply(pd.to_numeric, errors='coerce')
+  df['paon'] = df['paon'].apply(pd.to_numeric, errors='ignore')
   # convert date
   df['date'] = pd.to_datetime(df['date'])
   # convert types
   df['propertyType'] = df['propertyType'].apply(lambda x: x.split('/')[-1])
   df['estateType'] = df['estateType'].apply(lambda x: x.split('/')[-1])
   # add a nice address column
-  df['address'] = df['saon'].fillna('') + ' ' + df['paon'].map(str) + ' ' + df['street'].str.title()
+  flat_addr = np.where(df['saon'].isna(), '', df['saon'].map(str) + ' ')
+  df['address'] = flat_addr + df['paon'].map(str) + ' ' + df['street']
+  df['address'] = df['address'].str.title()
+  # reorder for better display
+  df = df[['address', 'amount', 'date', 'postcode', 'propertyType', 'estateType',
+          'saon', 'paon', 'street', 'town', 'county']]
   # sort by date
   df = df.sort_values(['street', 'paon', 'date'])
   return df
@@ -63,7 +77,7 @@ def get_multi_postcode_df(postcodes):
   return pd.concat(dataframes)
 
 
-def plot_from_df(df):
+def plot_from_df(df, title):
 
   fig = px.scatter(
     df,
@@ -74,29 +88,67 @@ def plot_from_df(df):
     hover_data=["postcode"],
     trendline="lowess",
     trendline_options=dict(frac=0.25),
-    title=postcode_str,
+    title=title,
     )
   fig.update_traces(marker=dict(size=10))
 
-  addresses = df.groupby('address')
-
-  # for name, addr in addresses:
-  #   fig.add_traces(
-  #       list(px.line(
-  #           addr,
-  #           x="date",
-  #           y="amount",
-  #       ).update_traces(opacity=0.15).select_traces())
-  #   )
   return fig
 
 # Now Streamlit
-postcode_str = st.text_input('Postcodes:')
+st.markdown(
+  """
+  Enter postcodes separated by commas. The space in the postcode is important!
+
+  Optionally enter a potential sale price to have it shown on the plot.
+  Optionally enter an address to highlight sales on the plot. Check the raw data 
+  below to get the right address format.
+
+  The plot is zoomable, and if you hover on a point you get the address.
+  """
+)
+postcode_str = st.sidebar.text_input('Postcodes:')
+st.sidebar.markdown("""---""")
+
+purchase_price = st.sidebar.number_input('Suggested Price:', format='%d', step=5000)
+purchase_addr = st.sidebar.text_input('Highlight Address:')
+st.sidebar.write("Filters:")
+type_checkboxes = [st.sidebar.checkbox(t, value=True) for t in property_types]
 
 if st.button('Get Data'):
   with st.spinner('Wait for it...'):
-    df = get_multi_postcode_df(postcode_str.split(', '))
-    fig = plot_from_df(df)
+    postcodes = [p.strip().upper() for p in postcode_str.split(',')]
+    df = get_multi_postcode_df(postcodes)
+
+    # filter according to property type
+    types = np.array(property_types)[type_checkboxes]
+    df_plot = df[df['propertyType'].isin(types)]
+
+    fig = plot_from_df(df_plot, ', '.join(postcodes))
+
+    if purchase_price:
+      # A marker for the price itself
+      fig.add_trace(
+          go.Scatter(x=[date.today()],
+                      y=[purchase_price],
+                      mode='markers',
+                      marker_color='yellow',
+                      marker_size=15,
+                      name='Suggested Price')
+      )
+
+    if purchase_addr:
+      # and highlight the chosen property
+      df_highlight = df_plot.query('address == @purchase_addr')
+      fig.add_trace(
+          go.Scatter(x=df_highlight['date'],
+                      y=df_highlight['amount'],
+                      mode='markers',
+                      marker_symbol='circle-open',
+                      marker_color='yellow',
+                      marker_size=15,
+                      name='Highlight Address')
+      )
+
     st.plotly_chart(fig)
     my_expander = st.expander("Data", expanded=False)
     with my_expander:
